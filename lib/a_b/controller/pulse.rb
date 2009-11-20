aeed3a7cb0c4a860c2dfc01bced0885b14a1d19f
winton <mail@wintoni.us>
2009-11-20 11:45:41 -0800

    Adding POST pulse

--- lib/a_b/controller/pulse.rb
+++ lib/a_b/controller/pulse.rb
@@ -4,4 +4,9 @@ Application.class_eval do
     rows = ActiveRecord::Base.connection.execute("select 1 from dual").num_rows rescue 0
     rows == 1 ? "OK" : "Error!"
   end
+  
+  post '/pulse' do
+    rows = ActiveRecord::Base.connection.execute("select 1 from dual").num_rows rescue 0
+    rows == 1 ? "OK" : "Error!"
+  end
 end
