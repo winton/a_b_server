a70a468303efa016fd231dcc5e7a2c1733d77cf5
winton <mail@wintoni.us>
2009-11-22 22:41:52 -0800

    Specs for the Token model

--- lib/a_b/model/token.rb
+++ lib/a_b/model/token.rb
@@ -3,22 +3,28 @@ class Token < ActiveRecord::Base
   set_table_name :a_b_tokens
   @@cached_at = nil
   
-  def self.cached
-    if @@cached_at.nil? || @@cached_at < Time.now.utc - 60 * 60
-      @@cached = Token.find(:all, :limit => 2, :order => 'id desc')
-      if @@cached.empty? || @@cached.first.created_at < Time.now.utc - 60 * 60
-        @@cached.pop
-        @@cached << generate
+  class <<self
+    def cached
+      if @@cached_at.nil? || (Time.now.utc - @@cached_at).to_i >= 60
+        @@cached = Token.find(:all, :limit => 2, :order => 'id desc')
+        if @@cached.empty? || (Time.now.utc - @@cached.first.created_at).to_i >= 60 * 60
+          @@cached.pop if @@cached.length > 1
+          @@cached.unshift generate
+        end
+        @@cached_at = Time.now.utc
+        @@cached = @@cached.collect &:token
       end
-      @@cached_at = @@cached.first.created_at
-      @@cached = @@cached.collect &:token
+      @@cached
     end
-    @@cached
-  end
   
-  private
+    private
   
-  def self.generate
-    self.create :token => Authlogic::Random.friendly_token
+    def generate
+      self.create :token => Authlogic::Random.friendly_token
+    end
+    
+    def reset
+      @@cached_at = nil
+    end
   end
 end
