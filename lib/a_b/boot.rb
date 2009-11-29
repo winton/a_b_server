4b1c7ff912a03520f749a3c286520a9a9908d08c
Winton Welsh <winton@Winton-Welshs-MacBook-Pro-17.local>
2009-11-29 03:24:03 -0800

    Sinatra options for rack_hoptoad

--- lib/a_b/boot.rb
+++ lib/a_b/boot.rb
@@ -2,10 +2,10 @@ Application.class_eval do
   
   # Sinatra
   enable :raise_errors
+  disable :logging, :show_exceptions
   set :environment, $testing ? :test : environment
   set :root, File.expand_path("#{File.dirname(__FILE__)}/../../")
   set :public, "#{root}/public"
-  set :logging, true
   set :static, true
   set :views, "#{root}/lib/a_b/view"
     
@@ -28,6 +28,6 @@ Application.class_eval do
   
   # Hoptoad notifier
   if File.exists?(hoptoad = "#{root}/config/hoptoad.txt")
-    use Rack::HoptoadNotifier, File.read(hoptoad)
+    use Rack::HoptoadNotifier, File.read(hoptoad).strip
   end
 end
