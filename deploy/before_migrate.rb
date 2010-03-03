5f6febf9b983969d09946238e7a873609ed0eaae
Tung Nguyen <tongueroo@gmail.com>
2010-03-02 23:09:06 -0800

    before migrate hook : install pre-requisite gem for require

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,12 @@
 # chef hook for eycloud env
 
 if %w(app_master app util).include?(node[:instance_role])
+  # prerequire gems for require
+  sudo "gem install active_wrapper --version='0.2.4' --no-ri --no-rdoc"
+  sudo "gem install rake --version=0.8.7 --no-ri --no-rdoc"
+  sudo "gem install rspec --version=1.3.0 --no-ri --no-rdoc"
+  sudo "gem install require --no-ri --no-rdoc"
+  
+  # gems for app
   sudo "rake gems:install SUDO=1 DOCS=0"
 end
