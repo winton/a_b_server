9e2dddabceb4d2df3abb175b607f8d19ffb04809
Tung Nguyen <tongueroo@gmail.com>
2010-03-10 15:05:07 -0800

    update required gems to install

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,12 +1,6 @@
 # chef hook for eycloud env
 
 if %w(app_master app util solo).include?(node[:instance_role])
-  # prerequire gems for require
-  sudo "gem install active_wrapper --version='0.2.4' --no-ri --no-rdoc"
-  sudo "gem install rake --version=0.8.7 --no-ri --no-rdoc"
-  sudo "gem install rspec --version=1.3.0 --no-ri --no-rdoc"
-  sudo "gem install require --no-ri --no-rdoc"
-  
   # gems for app
   sudo "cd #{latest_release} && rake gems:install SUDO=1 DOCS=0"
 end
