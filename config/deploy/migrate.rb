845f7ac31930283557b6d0b6255b500dd2c43b20
Tung Nguyen <tongueroo@gmail.com>
2010-03-11 11:56:17 -0800

    move accidentally checked in deploy files

--- config/deploy/migrate.rb
+++
@@ -1,6 +0,0 @@
-# override capistrano's deploy:migrate because we need to use RACK_ENV vs RAILS_ENV
-namespace :deploy do
-  task :migrate, :roles => :db, :only => { :primary => true } do
-    run "cd #{latest_release}; rake RACK_ENV=#{rack_env} db:migrate"
-  end
-end
