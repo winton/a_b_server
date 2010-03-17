a6a3450bcf938af66f81c18ba741efa328037f84
Tung Nguyen <tongueroo@gmail.com>
2010-03-17 15:07:55 -0700

    add newrelic symlink

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,24 @@
 # chef hook for eycloud env
 
+def do_symlinks(folder)
+  normal_symlinks = %w(
+    config/newrelic.yml
+  )
+  commands = []
+  commands << normal_symlinks.map do |path|
+    "rm -rf #{folder}/#{path} && \
+     ln -sf #{shared_path}/#{path} #{folder}/#{path}"
+  end
+  run <<-CMD
+    cd #{folder} &&
+    #{commands.join(" && ")}
+  CMD
+end
+
 if %w(app_master app util solo).include?(node[:instance_role])
   # gems for app
   sudo "cd #{latest_release} && rake gems:install SUDO=1 DOCS=0"
+  
+  # symlinks
+  do_symlinks(latest_release)
 end
