49bd614a2f07fcc4c5b6d6ebca9b8b01d6aa2a11
winton <mail@wintoni.us>
2010-10-19 16:42:51 -0700

    Adding better Delayed::Worker options

--- lib/a_b/boot/delayed_job.rb
+++ lib/a_b/boot/delayed_job.rb
@@ -1,2 +1,7 @@
 $:.unshift File.expand_path('../../../../vendor/delayed_job/lib', __FILE__)
 require 'delayed_job'
+
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 10
+Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_run_time = 10.minutes
